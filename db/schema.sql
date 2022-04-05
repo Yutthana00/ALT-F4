ffca689326b97422fc2c082e3b3afbe2041717b6
roconn <roconn25@gmail.com>
2022-04-05 21:19:27 +0100

    connection.js + schema.sql + userModel complete

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS gamer_db;
+CREATE DATABASE gamer_db;
---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize')
+const bcrypt = require('bcrypt')
+const sequelize = require('../config/connection')
+
+
+class User extends Model {
+    checkPassword(loginPass) {
+      return bcrypt.compareSync(loginPass, this.password);
+    }
+  }
+  
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+      },
+        reviews: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6],
+        },
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 15);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 15);
+          return updatedUserData;
+        },
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
+  module.exports = User;
