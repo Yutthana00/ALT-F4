b4ed4db5e39f84ac40146489ca6189e0b4761959
roconn <roconn25@gmail.com>
2022-04-07 21:27:00 +0100

    model index.js completed

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User')
+const Review = require('./Review')
+const Game = require('./Game')
+const Comment = require('./Comment')
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(Review, {
+    foreignKey: 'review_id',
+    onDelete: 'CASCADE'
+})
+
+Review.hasMany(Game, {
+    foreignKey: 'review_id',
+    onDelete: 'CASCADE'
+})
+
+Game.hasMany(Review, {
+    foreignKey: 'game_id',
+    onDelete: 'CASCADE'
+})
+
