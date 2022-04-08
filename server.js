965df617e8df4ecd384aa8f7c84763b05a0c8c52
Yutthana00 <arf101293@gmail.com>
2022-04-08 02:40:56 +0100

    Server is now running from this change ****

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+app.listen(PORT, () => {
+  console.log(`Now listening on port ${PORT}!`);
 });
