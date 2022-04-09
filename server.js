146eab3180071f3d596dc043c805d3c176930ef1
roconn <roconn25@gmail.com>
2022-04-09 11:04:53 +0100

    changed app.listen to sequelize.sync in server

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(routes);
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Now listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening!'));
 });
