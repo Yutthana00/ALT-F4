91c748bcaa7d4f95aeeeab5b3c0b83357683f59d
roconn <roconn25@gmail.com>
2022-04-09 11:06:42 +0100

    connection.js changed to local host + port 3001

--- config/connection.js
+++ config/connection.js
@@ -14,9 +14,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "127.0.0.1",
+      host: "localhost",
       dialect: "mysql",
-      port: 3306,
+      port: 3001,
     }
   );
 }
