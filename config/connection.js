c6b161540d1d80374823afdb14ebe3184de3ddc6
roconn <roconn25@gmail.com>
2022-04-09 11:08:15 +0100

    reverted connection.js changes

--- config/connection.js
+++ config/connection.js
@@ -14,9 +14,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "localhost",
+      host: "127.0.0.1",
       dialect: "mysql",
-      port: 3001,
+      port: 3306,
     }
   );
 }
