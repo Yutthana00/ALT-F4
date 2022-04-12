0788a6cb02fb4997b6d2c1b5dbd2e0cfdb41e5ad
roconn <roconn25@gmail.com>
2022-04-12 22:20:14 +0100

    gameroutes seperated into igdb.js

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -3,37 +3,68 @@ const router = express.Router()
 require("dotenv").config();
 const axios = require('axios');
 const { response } = require('express');
+const { searchForGame, homePageGames } = require('../../lib/igdb')
+
+// router.post('/:search', async (req, res) => {
+
+// const userSearch = req.params.search
+// // we don't have the search yet
+
+//     try {
+//         axios({
+//             url: `https://api.igdb.com/v4/games/${userSearch}`,
+//             method: 'POST',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Client-ID': process.env.client_id,
+//                 'Authorization': process.env.authorization,
+//             },
+//             data: "fields id, cover, game_modes, genres, release_dates, screenshots, platforms, summary;"
+//           })
+//     .then((response) => {
+//         console.log(response.data)
+//         res.json(response.data)
+//     })
+   
+// } catch (err) {
+//         console.log(err)
+//     }
+// } 
+// )
 
 
-// this does a thing and im writing it cause yasmin is telling me to
-// also it gets games and we need to pull the platform number into a seperate fetch to get the name of the platform!
 router.post('/:search', async (req, res) => {
 
-const userSearch = req.params.search
-// we don't have the search yet
-
+    const userSearch = req.params.search
+    // we don't have the search yet
+    
     try {
-        axios({
-            url: `https://api.igdb.com/v4/games/${userSearch}`,
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Client-ID': process.env.client_id,
-                'Authorization': process.env.authorization,
-            },
-            data: "fields id, cover, game_modes, genres, release_dates, screenshots, platforms, summary;"
-          })
-    .then((response) => {
-        console.log(response.data)
+        const response = await searchForGame(userSearch)
+        const games = response.data;
+
+        games.forEach(game => {
+            // Get platform names
+            // getPlatformNames(game.platforms)
+            // Get cover
+            // etc...
+        });
+
         res.json(response.data)
-    })
-   
-} catch (err) {
+       
+    } catch (err) {
         console.log(err)
     }
-} 
-)
+})
 
-router.post('/')
+router.post('/', async (req, res) => {
+    try {
+        const reponse = await homePageGames()
+        const games = response.data
+
+        // pull cover, slug
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 module.exports = router
