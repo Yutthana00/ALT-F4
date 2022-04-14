74361a158113b3ab1c901aeab2bb8165b8c858cb
roconn <roconn25@gmail.com>
2022-04-14 19:27:45 +0100

    user.delete added

--- controllers/api/dashRoutes.js
+++ controllers/api/dashRoutes.js
@@ -14,6 +14,7 @@ router.get('/', withAuth, async (req, res) => {
         })
 
     } catch (err) {
+        console.log(err)
         res.status(500).json(err)
     }
 })
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const { User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // Get ALL the users in our db
 router.get("/", async (req, res) => {
@@ -80,6 +81,25 @@ router.post("/logout", (req, res) => {
     res.status(404).end();
   }
 });
+
+
+router.delete('/deactive', withAuth, async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: { 
+                id: req.session.user_id
+            }
+        })
+
+        if (!withAuth) {
+            res.status(403).json({ message: 'You do not have permission to do this' })
+        }
+
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // Do we need this bit? !!!
 // router.post('/', async (req, res) => {
