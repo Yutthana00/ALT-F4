6bb26b69ddcaff1bcdcbf7413ffc2aae2c883f1c
Yasmin <yasmin.bunnie@gmail.com>
2022-04-14 20:34:57 +0100

    signup page small corrections on fetch

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -6,14 +6,14 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/users/signUp", {
       method: "POST",
       body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
