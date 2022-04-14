b36f237c3a1b90bac51f116f1b69e0367c4dff70
Yasmin <yasmin.bunnie@gmail.com>
2022-04-14 19:14:53 +0100

    moving dashboard.handlebars location

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,7 +27,7 @@ router.get("/dashboard", withAuth, async (req, res) => {
 
     const user = (userData = userData.get({ plain: true }));
 
-    res.render("profile", {
+    res.render("dashboard", {
       ...user,
       logged_in: true,
     });
