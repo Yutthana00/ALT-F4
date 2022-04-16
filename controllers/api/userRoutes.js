24f82796bab21ef979a69d4f3e7f163e88979d26
roconn <roconn25@gmail.com>
2022-04-16 03:13:41 +0100

    styling edits

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -105,20 +105,5 @@ router.delete('/deactive', withAuth, async (req, res) => {
     }
 })
 
-// Do we need this bit? !!!
-// router.post('/', async (req, res) => {
-//     try {
-//       const userData = await User.create(req.body);
-
-//       req.session.save(() => {
-//         req.session.user_id = userData.id;
-//         req.session.logged_in = true;
-
-//         res.status(200).json(userData);
-//       });
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
 
 module.exports = router;
